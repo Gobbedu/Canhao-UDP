cc28cff4c3ff557eb24bf1f469de8781b396e671
Gobbedu <gobbolegal@gmail.com>
2023-02-06 16:43:08 -0300

    mantem tema config.toml consistente

--- site-relatorio-canhao/update_gihub_site.sh
+++ site-relatorio-canhao/update_gihub_site.sh
@@ -2,4 +2,8 @@
 
 rm -rf ../docs/*; # remove os arquivos atuais do github
 hugo --minify --theme hugo-book; # faz o build do site 
-cp -vR public/* ../docs/*; # copia os arquivos para o site github
+mv public/ ../docs/; # move os arquivos da pasta public para o github
+
+
+# cp -vR public/* ../docs/*; # copia os arquivos para o site github
+# rm -rf public/
