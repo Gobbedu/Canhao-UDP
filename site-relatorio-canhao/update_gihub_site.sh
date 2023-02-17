092c1a466e16726a00752ec4cb2ee96ae0d9141c
Gobbedu <gobbolegal@gmail.com>
2023-02-16 21:30:10 -0300

    adiciona site publico

--- site-relatorio-canhao/update_gihub_site.sh
+++ site-relatorio-canhao/update_gihub_site.sh
@@ -2,7 +2,7 @@
 
 rm -rf ../docs/*; # remove os arquivos atuais do github
 hugo --minify --theme hugo-book; # faz o build do site 
-mv public/ ../docs/; # move os arquivos da pasta public para o github
+mv public/* ../docs/; # move os arquivos da pasta public para o github
 
 
 # cp -vR public/* ../docs/*; # copia os arquivos para o site github
